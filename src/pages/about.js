11309ee8e1c9a328dc9c375c851cda7af2c3bb15
clamba15 <cl@customcraftsoftware.com>
2024-10-13 00:05:58 -0700

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,14 +14,7 @@ const About = () => (
     
     <section className="about-section">
       <h1 className="about-title">About Us</h1>
-      <p className="about-text">
-        Welcome to Your Business Name. We are dedicated to delivering top-quality services to our clients.
-        Our mission is to help businesses succeed by providing professional solutions tailored to their needs.
-      </p>
-      <p className="about-text">
-        Our team is composed of passionate individuals who bring a wealth of experience and expertise to the table.
-        Whether you're looking for service 1, service 2, or service 3, we are here to help you achieve your goals.
-      </p>
+      
     </section>
   </Layout>
 );
